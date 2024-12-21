1f060a1a1dbb7a366d282d2fe4a9b2c9f75745e6
aphsx <168500857+aphsx@users.noreply.github.com>
2024-12-21 14:01:31 +0700

    Add new projects to projectsData; include descriptions, technologies, and images

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -9,7 +9,43 @@ const projects = [
           image: "/images/portfolio.png", // Replace with the actual path to your image
           link: "#",
         },
-  ];
-  
+        {
+          title: "CoSI Public Web",
+          year: 2023,
+          description: `
+            Developed a public-facing web application for CoSI within 2 months. Features include:
+            - News overview page
+            - Detailed news content page
+            - CoSI members list page
+            - Detailed member information page
+          `,
+          technologies: ["Next.js", "React", "Tailwind CSS"],
+          category: "Web Development",
+          image: "/images/CoSI Overall news.png", // Replace with the actual path to your image
+          link: "#",
+        },
+        {
+          title: "CoSI Member Web",
+          year: 2023,
+          description:
+            "Built a members-only web application for CoSI within 2 months. Key features include:\
+            - Light and Dark theme support\
+            - CRUD functionality for managing CoSI internships",
+          technologies: ["Next.js", "React", "Tailwind CSS"],
+          category: "Web Development",
+          image: "/images/CoSI Member web.png", // Replace with the actual path to your image
+          link: "#",
+        },
+        {
+          title: "E-MED Synphaet",
+          year: 2023,
+          description:
+          "Delivered a web application for Synphaet E-MED in just 1 week. Features include:\n- Course overview page\n- Detailed course information page",
+          technologies: ["Next.js", "React", "Tailwind CSS"],
+          category: "Web Development",
+          image: "/images/EmedProject.png", // Replace with the actual path to your image
+          link: "#",
+        },
+      ];
   export default projects;
   
