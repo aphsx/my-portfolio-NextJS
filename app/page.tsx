adcacc39ad6baf1b55b4241ab24dca846932ff45
aphsx <168500857+aphsx@users.noreply.github.com>
2025-02-16 09:18:07 +0700

    Comment out Education and AboutMe components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import ProfileSection from "@/components/ProfileSection";
 import Hero from "@/components/AboutSection";
 import SkillsAndFrameworks from "@/components/Skills";
-import Education from "@/components/Education";
-import AboutMe from "@/components/AboutMe";
+// import Education from "@/components/Education";
+// import AboutMe from "@/components/AboutMe";
 export default function Home() {
   return (
     <div className="mx-auto max-w-2xl px-4 sm:px-6 xl:px-0 space-y-10">
