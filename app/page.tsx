3f2d2a4e9d7be57890d6553ae2d706b31d8548d6
aphsx <168500857+aphsx@users.noreply.github.com>
2025-02-16 09:17:55 +0700

    Comment out Education and AboutMe components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <ProfileSection />
       <Hero />
       <SkillsAndFrameworks />
-      <Education />
-      <AboutMe />
+      {/* <Education />
+      <AboutMe /> */}
     </div>
   );
 }
